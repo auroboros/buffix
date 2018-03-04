fdcb7eb75aed52467e07076b22fa2587e798c0bc
John McGill <jmcg.music@gmail.com>
2018-03-03 23:57:58 -0500

    try fixed buffer size

--- src/main/scala/AudioConfig.scala
+++ src/main/scala/AudioConfig.scala
@@ -6,15 +6,17 @@ import javax.sound.sampled.AudioFormat
 object AudioConfig {
   val samplingRate = 44100 // TODO
   val nChannels = 1 // TODO
-  val USE_BIG_ENDIAN = false // TODO
+  val USE_BIG_ENDIAN = true // TODO
 
   val format = new AudioFormat(samplingRate, 16, nChannels, true, USE_BIG_ENDIAN)
 
-  val inBufferSize: Int = calculateBufferSize(AudioSystemConstants.suggestedInputLatency, AudioConfig.format)
-  val outBufferSize: Int = calculateBufferSize(AudioSystemConstants.suggestedOutputLatency, AudioConfig.format)
+  val TempBufferSizeConstant = 1024
+
+  val inBufferSize: Int =  TempBufferSizeConstant // calculateBufferSize(AudioSystemConstants.suggestedInputLatency, AudioConfig.format)
+  val outBufferSize: Int = TempBufferSizeConstant // calculateBufferSize(AudioSystemConstants.suggestedOutputLatency, AudioConfig.format)
 
   private def calculateBufferSize(suggestedOutputLatency: Double, format: AudioFormat): Int = {
-    val numFrames: Int = (suggestedOutputLatency * format.getFrameRate).toInt
+    val numFrames: Int = (suggestedOutputLatency * samplingRate).toInt
     val numBytes: Int = numFrames * AudioConfig.nChannels * AudioSystemConstants.BYTES_PER_SAMPLE
 
     numBytes
