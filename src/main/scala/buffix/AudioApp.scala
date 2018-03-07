ed315a34fe7925dd66fb22252f00e310d4eb9e85
John McGill <jmcg.music@gmail.com>
2018-03-06 22:54:33 -0500

    start quarantining demo code, have outbuffer built into SimpleAudioSystem (managed IO)

--- src/main/scala/buffix/AudioApp.scala
+++ src/main/scala/buffix/AudioApp.scala
@@ -1,46 +1,15 @@
 package buffix
 
-import javax.sound.sampled.{AudioSystem, DataLine, Line}
-
-import buffix.ugen.{Square, Sawtooth, Sine}
-
+import buffix.ugen.Sawtooth
+import buffix.SimpleAudioSystem.write
 /**
   * Created by johnmcgill on 3/3/18.
   */
 object AudioApp extends App {
-  implicit def richBufferToByteArray(richBuffer: IoBuffer): Array[Byte] = richBuffer.buffer
-
-  val mixerInfos = AudioSystem.getMixerInfo
-
-  mixerInfos.foreach(println)
-
-  val mixers = mixerInfos.map(AudioSystem.getMixer)
-
-  val defaultInputDevice = mixers.find(mixer => channelsAreAvailable(mixer.getTargetLineInfo))
-  val defaultOutputDevice = mixers.find(mixer => channelsAreAvailable(mixer.getSourceLineInfo))
-
-  val lineOut = LineManager.startOutputStream().get
-  val lineIn = LineManager.startInputStream().get
-
-  println("writing noise in a loop")
-
   val sineGen = Sawtooth()
-
-  val outBuffer = IoBuffer()
 
   1 to Int.MaxValue foreach(_ => {
     sineGen.nextBuffer()
-    sineGen.signalBuffer.copyToRichBuffer(outBuffer)
-    lineOut.write(outBuffer, 0, outBuffer.length)
+    write(sineGen.signalBuffer)
   })
-
-  private def channelsAreAvailable(lines: Array[Line.Info]): Boolean = scanMaxChannels(lines) > 0
-
-  private def scanMaxChannels(lines: Array[Line.Info]): Int =
-    (0 +: lines.map {
-      case info: DataLine.Info => scanMaxChannels(info)
-      case _ => 0
-    }).max
-
-  private def scanMaxChannels(info: DataLine.Info): Int = (0 +: info.getFormats.map(_.getChannels)).max
 }
--- src/main/scala/buffix/ugen/Phasor.scala
+++ src/main/scala/buffix/ugen/Phasor.scala
@@ -6,8 +6,7 @@ import buffix.AudioConfig
   * Created by johnmcgill on 3/5/18.
   */
 object Phasor {
-  val TwoPi = Math.PI * 2
-  val Period = TwoPi
+  val Period = Math.PI * 2
 
   def computeW(pitch: Int, samplingRate: Int): Double = Period * pitch / samplingRate
 }
